298f3245c4f055d2871d499bdf06e22d690ccc1d
Alessia <alessia1.testa@mail.polimi.it>
2025-03-18 16:53:16 +0100

    Commit to write the first case in StepDefinitions.java

--- project-healthcalc/src/test/java/healthcalc/bdd/StepDefinitions.java
+++ project-healthcalc/src/test/java/healthcalc/bdd/StepDefinitions.java
@@ -3,21 +3,35 @@ package healthcalc.bdd;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.en.*;
+import static org.junit.Assert.*;
 
 public class StepDefinitions {
-@Given("I have a health calculator")
-public void i_have_a_health_calculator() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+
+    private HealthCalcImpl healthCalc;
+    private float result;
+    private Exception exception;
+
+    @Given("I have a health calculator")
+    public void i_have_a_health_calculator() {
+        // Arrange: Instantiate the health calculator
+        healthCalc = new HealthCalcImpl();
+    }
+    @When("I enter a height of {int} cm and a man gender")
+    public void i_enter_a_height_of_cm_and_a_man_gender(Integer int1) {
+        // Act: Call the idealWeight method for a man
+        try {
+            result = healthCalc.idealWeight(int1, 'm');
+        } catch (Exception e) {
+            exception = e;
+        }
+    }
+    @Then("The system returns an ideal weight of {double}")
+    public void the_system_returns_an_ideal_weight_of(Double double1) {
+        // Assert: Check if the calculated weight matches the expected value
+        assertEquals(double1, result, 0.01);
+    }   
 }
-@When("I enter a height of {int} cm and a man gender")
-public void i_enter_a_height_of_cm_and_a_man_gender(Integer int1) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-@Then("The system returns an ideal weight of {double}")
-public void the_system_returns_an_ideal_weight_of(Double double1) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}   
-}
+
+
+
