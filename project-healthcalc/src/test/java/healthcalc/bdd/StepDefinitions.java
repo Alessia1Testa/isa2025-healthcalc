6062062af950e40e68d185b2b7932733bce1e259
Alessia <alessia1.testa@mail.polimi.it>
2025-03-18 14:30:11 +0100

    Commit to write in StepDefinitions.java

---
+++ project-healthcalc/src/test/java/healthcalc/bdd/StepDefinitions.java
@@ -0,0 +1,23 @@
+package healthcalc.bdd;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+@Given("I have a health calculator")
+public void i_have_a_health_calculator() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@When("I enter a height of {int} cm and a man gender")
+public void i_enter_a_height_of_cm_and_a_man_gender(Integer int1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@Then("The system returns an ideal weight of {double}")
+public void the_system_returns_an_ideal_weight_of(Double double1) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}   
+}
