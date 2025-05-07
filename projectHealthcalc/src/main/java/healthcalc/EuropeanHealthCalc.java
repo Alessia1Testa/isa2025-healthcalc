7749da95131489cb571e2dff4a537e9685e7f5ff
Alessia <alessia1.testa@mail.polimi.it>
2025-05-07 15:59:05 +0200

    add: commit to add EuropeanHealthCalc.java

---
+++ projectHealthcalc/src/main/java/healthcalc/EuropeanHealthCalc.java
@@ -0,0 +1,14 @@
+package healthcalc;
+
+public class EuropeanHealthCalc extends HealthHospitalDecorator {
+    public EuropeanHealthCalc(HealthHospital wrappee) {
+        super(wrappee);
+    }
+
+    @Override
+    public double bmr(char genero, int edad, float alturaMetros, int pesoGramos) {
+        float alturaCm = alturaMetros * 100;
+        int pesoKg = pesoGramos / 1000;
+        return super.bmr(genero, edad, alturaCm, pesoKg);
+    }
+}
