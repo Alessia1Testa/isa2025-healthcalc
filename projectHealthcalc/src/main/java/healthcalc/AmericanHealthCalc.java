7e38889d2e025efe13d658b1ac75b8a37e52cfa5
Alessia <alessia1.testa@mail.polimi.it>
2025-05-07 16:00:18 +0200

    add: commit to add AmericanHealthCalc.java

---
+++ projectHealthcalc/src/main/java/healthcalc/AmericanHealthCalc.java
@@ -0,0 +1,14 @@
+package healthcalc;
+
+public class AmericanHealthCalc extends HealthHospitalDecorator {
+    public AmericanHealthCalc(HealthHospital wrappee) {
+        super(wrappee);
+    }
+
+    @Override
+    public double bmr(char genero, int edad, float alturaPies, int pesoLibras) {
+        float alturaCm = alturaPies * 30.48f;
+        int pesoKg = (int) (pesoLibras / 2.20462);
+        return super.bmr(genero, edad, alturaCm, pesoKg);
+    }
+}
