180736ef574610d76b52295d57628e143d7b04bd
Alessia <alessia1.testa@mail.polimi.it>
2025-05-20 17:50:02 +0200

    fix: commit to create an example in MainCalc.java

--- projectHealthcalc/src/main/java/healthcalc/MainCalc.java
+++ projectHealthcalc/src/main/java/healthcalc/MainCalc.java
@@ -7,18 +7,25 @@ public class MainCalc {
 
 
 public static void main(String[] args) {
+
 	HealthCalc model = HealthCalcImpl.getInstance();
 
     
     HealthHospital adapter = new HealthCalcAdapter(model);
 
-	HealthStats proxy = new HealthCalcProxy(adapter);
+    HealthStats proxy = new HealthCalcProxy(adapter);
     
-	HealthHospital hospital = (HealthHospital) proxy;
+    HealthHospital hospital = (HealthHospital) proxy;
 
-	HealthHospital european = new EuropeanHealthCalc(adapter);
+    HealthHospital european = new EuropeanHealthCalc(adapter);
 
-	HealthHospital englishCalc = new BMREngMessageDecorator(european);
+    HealthHospital englishCalc = new BMREngMessageDecorator(adapter);
+
+    double bmrValue = englishCalc.bmr('m', 30, 1.70f, 70000);
+
+    System.out.println("BMR Value: " + bmrValue);
+
+	
 
 	}
 }
